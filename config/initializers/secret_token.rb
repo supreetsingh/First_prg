ac6417540b44e697cb5dde4c19397e4ef7e14124
Virtual Machine Administrator <administrator@ubuntu-ror-dev-env.(none)>
2011-10-19 12:22:25 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstPrg::Application.config.secret_token = 'ee053994f94ac283470f0d6e1e7d67d92d06a9883178a63e4a04a0be0cfce10848a36a21d85ce87246417fa28736e871341e47c6f0713efbc9707215b4d07598'
